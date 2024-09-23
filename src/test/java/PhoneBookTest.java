a0c8041d2acb2b07f9fc492c61a637f3c9b296a2
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-09-23 18:17:35 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void addContactTest(){
+        phoneBook = new PhoneBook();
+        for (int i = 0; i < 10; i++) {
+            int count = phoneBook.add("Name " + i + 1, "Number " + i + 1);
+            assertEquals(i+1, count, "Contact count should be "
+                    + (i+1) + " after adding a contact " + i);
+        }
+    }
 }
